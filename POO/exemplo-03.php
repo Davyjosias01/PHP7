e63486768861e7226bd97589c7f2e85275245b9f
Davyjosias01 <davy.josias@gmail.com>
2023-06-02 11:48:15 -0300

    Primeiro commit nova branch

--- POO/exemplo-03.php
+++ POO/exemplo-03.php
@@ -10,63 +10,73 @@ class Documento{
 	}
 
 	public function setNumero($numero){
-		$this->cpf = $numero;
+        $resultado = Documento::validaCPF($numero);
+		
+        if ($resultado === false){
+            throw new Exception("CPF informado não é válido", 1);
+        }
+        $this->cpf = $numero;
 	}
 
-	public function validaCPF($cpf){
+	public static function validaCPF($cpf):bool{
 
-		 if(empty($cpf)) {
-        return false;
-    }
+
+        //Inicio da função valida CPF, código copiado - - - - - - 
+        if(empty($cpf)) {
+            return false;
+        }
     
-    $cpf = preg_match('/[0-9]/', $cpf)?$cpf:0;
+        $cpf = preg_match('/[0-9]/', $cpf)?$cpf:0;
 
-    $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
      
     
-    if (strlen($cpf) != 11) {
-        echo "length";
-        return false;
-    }
+        if (strlen($cpf) != 11) {
+            echo "length";
+            return false;
+        }
     
-    else if ($cpf == '00000000000' || 
-        $cpf == '11111111111' || 
-        $cpf == '22222222222' || 
-        $cpf == '33333333333' || 
-        $cpf == '44444444444' || 
-        $cpf == '55555555555' || 
-        $cpf == '66666666666' || 
-        $cpf == '77777777777' || 
-        $cpf == '88888888888' || 
-        $cpf == '99999999999') {
-        return false;
+        else if ($cpf == '00000000000' || 
+            $cpf == '11111111111' || 
+            $cpf == '22222222222' || 
+            $cpf == '33333333333' || 
+            $cpf == '44444444444' || 
+            $cpf == '55555555555' || 
+            $cpf == '66666666666' || 
+            $cpf == '77777777777' || 
+            $cpf == '88888888888' || 
+            $cpf == '99999999999') {
+            return false;
 
-     } else {   
-         
+        } else {     
+
         for ($t = 9; $t < 11; $t++) {
-             
+    
             for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf{$c} * (($t + 1) - $c);
+                $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
-            if ($cpf{$c} != $d) {
-                return false;
-            }
+    
+                if ($cpf[$c] != $d) {
+                    return false;
+                }
         }
- 
         return true;
     }
-
 	}
-
+    //Fim da função valida CPF. - - - - - - - - - 
 }
+//Fim da class Documento - - - - - - - - -  
 
+/*
 $cpf = new Documento();
 
-$cpf->setNumero("123456789");
+$cpf->setNumero("11015001904");
 
 var_dump($cpf->getNumero());
+*/
 
+var_dump(Documento::validaCPF("11015001904"));
 
 
 
---
+++ POO/exemplo-04.php
@@ -0,0 +1,38 @@
+<?php  
+    //Cada método mágico possui um objetivo.
+
+class Endereco {
+    
+    private $logradouro;
+    private $numero;
+    private $cidade;
+    
+    /*__construct é executado toda vez que um objeto é criado
+    ou, cada vez que uma instância é criada.*/
+    public function __construct($a, $b, $c){
+        $this->logradouro=$a;
+        $this->numero=$b;
+        $this->cidade=$c;
+    }
+
+
+    /*__destruct é executado toda vez que o código termina,
+    no caso, toda vez que ele */
+    public function __destruct(){
+        echo("<br><br>DESTRUIU");
+    }
+
+    public function __toString(){
+        return 
+        $this->logradouro.", nº".
+        $this->numero ." - ". 
+        $this->cidade;
+    }
+}
+
+$meuEndereco = new Endereco("Rua São Bento", 87, "Luzerna");
+
+echo $meuEndereco;
+
+
+?>
