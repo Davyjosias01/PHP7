46d939c1c8ff99f68649935705edf08fd4a0b1cf
Davyjosias01 <davy.josias@gmail.com>
2023-06-05 11:42:22 -0300

    dia 05-06-20230

--- heranca/exemplo-01.php
+++ heranca/exemplo-01.php
@@ -1,12 +1,56 @@
 <?php 
 
-class Pessoa {
+class Documento {
 
+	protected $numero;
 
+	public function getNumero(){
+		return $this->numero;
+	} 
+
+	public function setNumero($n){
+		$this->numero = $n;
+	} 		
 }
 
+class CPF extends Documento {
 
+	public function validar($cpf){ 
+	    // Extrai somente os números
+	    $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+	     
+	    // Verifica se foi informado todos os digitos corretamente
+	    if (strlen($cpf) != 11) {
+	        return false;
+	    }
 
+	    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+	    if (preg_match('/(\d)\1{10}/', $cpf)) {
+	        return false;
+	    }
 
+	    // Faz o calculo para validar o CPF
+	    for ($t = 9; $t < 11; $t++) {
+	        for ($d = 0, $c = 0; $c < $t; $c++) {
+	            $d += $cpf[$c] * (($t + 1) - $c);
+	        }
+	        $d = ((10 * $d) % 11) % 10;
+	        if ($cpf[$c] != $d) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+}
+
+$doc = new CPF;
+
+$doc -> setNumero('000000000-04');
+
+var_dump($doc -> validar($doc->getNumero()));
+
+echo "<br>";
+
+echo $doc -> getNumero();
 
 ?>
---
+++ interface/exemplo-01.php
@@ -0,0 +1,32 @@
+<?php 
+
+interface Veiculo {
+
+	public function acelerar($velocidade);
+	public function frear($velocidade);
+	public function trocaMarcha($marcha);
+
+}
+
+class Civic implements Veiculo {
+
+	public function acelerar($velocidade){
+		echo ("O veículo acelerou até: ". $velocidade . "km/h");
+	}
+
+	public function frear($velocidade){
+		echo ("O veículo freou até". $velocidade . "km/h");
+	}
+
+	public function trocaMarcha($marcha){
+		echo ("O veículo engatou a marcha: ". $marcha);
+	}
+}
+
+
+$carro = new Civic;
+
+$carro -> trocaMarcha(16);
+
+
+?>
